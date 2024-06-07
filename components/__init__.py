7c45696855d6e02561fd6cb45700dd8de5ae2b0a
vishakh prakash <vishakhpro2002@gmail.com>
2024-06-08 02:47:34 +0530

    FEAT: - creation of __init__.py

--- components/__init__.py
+++ components/__init__.py
@@ -1,3 +1,6 @@
 # this is __init__.py file in the same directory there is imageviewer.py file which has a class ImageSelector 
 
 from .ImageViewer import ImageSelector
+from .SidePanel import SidePanel
+from .WorkPanel import WorkPanel
+from .MainWidget import MainWidget
