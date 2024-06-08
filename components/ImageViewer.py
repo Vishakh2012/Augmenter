7f4b892e5f2d33a6eb5496cb6b55adbf2638181d
vishakh prakash <vishakhpro2002@gmail.com>
2024-06-09 02:16:38 +0530

    FEAT: - add image selection dialog box to the rotation tab - add rotate transformation to the rotation tab
    REFACTOR:
    - change logic of rotation of image out of the imageviewer

--- components/ImageViewer.py
+++ components/ImageViewer.py
@@ -18,12 +18,10 @@ class ImageSelector(QWidget):
         layout.addWidget(self.button)
         self.button.clicked.connect(self.select_image)
         self.setLayout(layout)
-        self.button = QPushButton("Rotate Image") 
-        self.button.clicked.connect(self.rotate_image)
         layout.addWidget(self.button)
         
 
-
+    getFileName = lambda self: self.file_name
     def select_image(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
@@ -36,38 +34,5 @@ class ImageSelector(QWidget):
             self.label.setPixmap(pixmap)
             self.label.setScaledContents(False)
 
-    def rotate_image(self):
-        if self.file_name:
-            rotated_image = Rotation().rotateByAngle(self.file_name, 90)
-            file_saved = self.save_image_component(rotated_image)
-            if(file_saved):
-                 pixmapx = QPixmap("rotated_image.jpg")
-                 pixmapx = pixmapx.scaled(400, 400)
-                 #show the image
-                 self.label.setPixmap(pixmapx)
+        
 
-            else:
-                 error_message = "Error in saving the file"
-                 print(error_message)
-
-        else:
-            print("Please select an image first")
-
-    def select_output_file_location(self):
-        options = QFileDialog.Options()
-        options |= QFileDialog.DontUseNativeDialog
-        file_name, _ = QFileDialog.getSaveFileName(self, "Save Image", "", "Image Files (*.png *.jpg *.jpeg *.bmp)", options=options)
-        return file_name
-    
-    def save_image_component(self, image: MatLike):
-        if self.file_name:
-            file_name = self.select_output_file_location()
-            if file_name:
-                print(file_name)
-                file_saved = file_interaction().save_image(file_name, image, "original")
-                if(file_saved):
-                    return True
-                else:
-                    return False
-        else:
-            print("Please select an image first")
--- components/Tabs/RotateTab.py
+++ components/Tabs/RotateTab.py
@@ -1,6 +1,10 @@
 
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel
-
+from PyQt5.QtWidgets import QPushButton, QWidget, QVBoxLayout, QLabel, QFileDialog
+from PyQt5.QtGui import QPixmap
+from image_transformation import Rotation
+from system_interaction import file_interaction
+from cv2.typing import MatLike
+from components import ImageSelector
 
 class RotateTab(QWidget):
 
@@ -8,8 +12,50 @@ class RotateTab(QWidget):
         super().__init__()
         main_layout = QVBoxLayout()
         main_layout.addWidget(QLabel('ROTATION'))
+        self.image_selector = ImageSelector()
+        main_layout.addWidget(self.image_selector)
         main_layout.addStretch(5)
+        self.button = QPushButton("Rotate")
+        self.button.clicked.connect(self.rotate_image)
+        main_layout.addWidget(self.button)
         self.setLayout(main_layout)
+        
 
+
+    def rotate_image(self):
+        if self.image_selector.getFileName():
+            rotated_image = Rotation().rotateByAngle(self.image_selector.getFileName(), 90)
+            file_saved = self.save_image_component(rotated_image)
+            if(file_saved):
+                 pixmapx = QPixmap("rotated_image.jpg")
+                 pixmapx = pixmapx.scaled(400, 400)
+                 #show the image
+                 self.label.setPixmap(pixmapx)
+
+            else:
+                 error_message = "Error in saving the file"
+                 print(error_message)
+
+        else:
+            print("Please select an image first")
+
+    def select_output_file_location(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        file_name, _ = QFileDialog.getSaveFileName(self, "Save Image", "", "Image Files (*.png *.jpg *.jpeg *.bmp)", options=options)
+        return file_name
+    
+    def save_image_component(self, image: MatLike):
+        if self.image_selector.getFileName():
+            file_name = self.select_output_file_location()
+            if file_name:
+                print(file_name)
+                file_saved = file_interaction().save_image(file_name, image, "original")
+                if(file_saved):
+                    return True
+                else:
+                    return False
+        else:
+            print("Please select an image first")
 
     
